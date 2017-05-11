24eaa4c5939a073f791afe5f2b16e83eb96c1505
karimabd84 <abdelkarim.esti@gmail.com>
2017-05-11 12:16:57 +0200

    1 commit

--- CordovaGooglePlusDemo/www/scripts/index.js
+++ CordovaGooglePlusDemo/www/scripts/index.js
@@ -18,7 +18,89 @@
         var receivedElement = parentElement.querySelector('.received');
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
+
+        document.getElementById('login').addEventListener('click', login.bind(this), false);
+
+        document.getElementById('logout').addEventListener('click', logout.bind(this), false);
+
+        document.getElementById('isAvailable').addEventListener('click', isAvailable.bind(this), false);
+
+        document.getElementById('trySilentLogin').addEventListener('click', trySilentLogin.bind(this), false);
+        document.getElementById('disconnect').addEventListener('click', disconnect.bind(this), false);
+        document.getElementById('getSigningCertificateFingerprint').addEventListener('click', getSigningCertificateFingerprint.bind(this), false);
+
     };
+
+    function login() {
+      window.plugins.googleplus.login(
+          {
+              // optional clientId of your Web application from Credentials settings of your project - On Android, this MUST be included to get an idToken. On iOS, it is not required.
+              'webClientId': '532680238703-buvcrqdvdo22q0i975urc063uq76ojfp.apps.googleusercontent.com'
+          },
+          function (obj) {
+            document.querySelector("#image").src = obj.imageUrl;
+            document.querySelector("#image").style.visibility = 'visible';
+            document.querySelector("#feedback").innerHTML = "Hi, " + obj.displayName + ", " + obj.email;
+            alert(JSON.stringify(obj));
+          },
+          function (msg) {
+              document.querySelector("#feedback").innerHTML = "error: " + msg;
+              alert('error: ' + msg);
+          }
+      );
+    }
+
+    function logout() {
+      window.plugins.googleplus.logout(
+          function (msg) {
+            document.querySelector("#image").style.visibility = 'hidden';
+            document.querySelector("#feedback").innerHTML = msg;
+          },
+          function (msg) {
+            document.querySelector("#feedback").innerHTML = msg;
+          }
+      );
+    }
+
+    function isAvailable() {
+      window.plugins.googleplus.isAvailable(function(avail) {alert(avail)});
+    }
+
+    function trySilentLogin() {
+        window.plugins.googleplus.trySilentLogin(
+            {
+                // optional clientId of your Web application from Credentials settings of your project - On Android, this MUST be included to get an idToken. On iOS, it is not required.
+                'webClientId': '532680238703-buvcrqdvdo22q0i975urc063uq76ojfp.apps.googleusercontent.com'
+            },
+            function (obj) {
+                document.querySelector("#image").src = obj.imageUrl;
+                document.querySelector("#image").style.visibility = 'visible';
+                document.querySelector("#feedback").innerHTML = "Silent hi, " + obj.displayName + ", " + obj.email;
+            },
+            function (msg) {
+                document.querySelector("#feedback").innerHTML = "error: " + msg;
+            }
+        );
+    };
+
+    function disconnect() {
+      window.plugins.googleplus.disconnect(
+          function (msg) {
+            document.queryselector("#image").style.visibility = 'hidden';
+            document.queryselector("#feedback").innerhtml = msg;
+          },
+          function (msg) {
+            document.queryselector("#feedback").innerhtml = msg;
+          }
+      );
+    }
+
+    function getSigningCertificateFingerprint() {
+        window.plugins.googleplus.getSigningCertificateFingerprint(
+            function (res) { alert(res) },
+            function (res) { alert(res) })
+    };
+
 
     function onPause() {
         // TODO: Diese Anwendung wurde ausgesetzt. Speichern Sie hier den Anwendungszustand.
